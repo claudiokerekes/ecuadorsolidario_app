2966636068085daa8e11f780c31aefc23187ac7d
Pelayo Maojo <pelayo.mao@gmail.com>
2016-06-07 23:59:56 +0100

    deploro

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,8 +1,8 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
+    user = User.find_or_create_by!(email: 'pelayo.mao@gmail.com') do |user|
+        user.password = '12341234'
+        user.password_confirmation = '12341234'
         user.confirm!
         user.admin!
       end
