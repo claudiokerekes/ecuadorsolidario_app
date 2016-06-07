40068a17b972122b8f97ba1a2cb5c35bef10c9b5
Pelayo Maojo <pelayo.mao@gmail.com>
2016-06-08 00:52:17 +0100

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,14 +73,14 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => Rails.application.secrets.email_provider_usernam,
-    :password             => Rails.application.secrets.email_provider_password,
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "23.246.221.198",
+    authentication: "plain",
+    :enable_starttls_auto => true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
   }
 
   # ActionMailer Config
