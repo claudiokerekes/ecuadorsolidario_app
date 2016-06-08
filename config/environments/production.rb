f6fcabc2b2f172c140579646909a71171520285e
Pelayo Maojo <pelayo.mao@gmail.com>
2016-06-08 01:01:21 +0100

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,10 +76,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "23.246.221.198",
-    authentication: "plain",
-    :enable_starttls_auto => true,
-    user_name: Rails.application.secrets.email_provider_username,
+    domain: "gmail.com",
+    user_name: "ecu.solidario@gmail.com",
     password: Rails.application.secrets.email_provider_password
   }
 
